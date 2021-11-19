4f755633cb1c672ce2670fa430a5c4f218510890
jinchoi <znyous@gmail.com>
2021-11-19 20:26:46 +0900

    #47 init

--- webserv/http/Response.cpp
+++ webserv/http/Response.cpp
@@ -1,9 +1,29 @@
 #include "Response.hpp"
 
-Response::Response(/* args */)
+Response::Response(void)
 {
 }
 
 Response::~Response()
 {
+}
+
+std::string Response::makeResponse() {
+  std::string ret = "";
+  ret += _makeStatusLine();
+  ret += _makeHeader();
+  ret += _makeBody();
+  return ret;
+}
+
+std::string Response::_makeStatusLine() {
+
+}
+
+std::string Response::_makeHeader() {
+
+}
+
+std::string Response::_makeBody() {
+
 }
--- webserv/http/Response.hpp
+++ webserv/http/Response.hpp
@@ -1,15 +1,44 @@
+#include <string>
+
 class Response
 {
   public:
-    Response(/* args */);
+    Response(void);
     ~Response();
+
+    std::string makeResponse();
+
+    int setHttpVersion(const int& n);
+    int setStatusCode(const int& n);
+    std::string setStatusMsg(const std::string& str);
+
   private:
+    std::string _makeStatusLine();
+    std::string _makeHeader();
+    std::string _makeBody();
+
+    int _httpVersion;
+    int _statusCode;
+    std::string _statusMsg;
+
+    int _contentLength;
 };
 
-Response::Response(/* args */)
-{
-}
+// [STATUS LINE]
+// int HTTP_version
+// int status_code
+// string status_msg
 
-Response::~Response()
-{
-}
+// [HEADER]
+// int HTTP_version
+// int status_code
+// string status_msg
+
+// int content_length
+// string server_name
+// string date
+// string content_type
+// string allow_method
+
+// [BODY]
+// string body

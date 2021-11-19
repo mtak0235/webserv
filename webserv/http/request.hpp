d4d0b19e36d8f582dafe8980900b1bec5d31b634
mtak0235 <jesustark0235@gmail.com>
2021-11-20 02:49:14 +0900

    client 응답시 무한루프 해결 - select

--- webserv/http/request.hpp
+++ webserv/http/request.hpp
@@ -1,44 +1,77 @@
 #include <string>
-
-class request
+#include <map>
+#include <cstring>
+enum http_method
 {
- private:
-  int _httpVersion;
-  std::string _statusMsg;
-  int _statusCode;
-
-
- public:
-    request(/* args */);
-  std::string _body;
-  std::string _raw
-  int
-
- public:
-    request(/* args */);
-    int get
-    
-    ~request();
+    GET= 0 ,
+    POST, 
+    DELETE
 };
 
-request::request(/* args */)
-    {
-
-}
-
-request::~request()
+class Request
 {
+    private:
+        std::string _raw;
+        std::string _rawFirstLine;
+        http_method _httpMethod;
+        std::string _path;
+        int _httpVersion;
+        std::string _rawHeader;
+        std::map<std::string, std::string> _headers;
+        std::string _body;
+
+    public:
+        Request();
+        virtual ~Request();
+        http_method getHttpMethod();
+        std::string getPath();
+        std::map<std::string, std::string> getHeaders();
+        std::string getBody();
+        void parseRequest(std::string);
+};
+
+Request::Request(/* args */)
+{
+    
 }
-rsion
-int status_code
-string status_msg
-string server_name
-string date
 
-string content_type
-int content_length
+Request::~Request()
+{
 
-string allow_method
+}
 
-[BODY]
-string body
+http_method Request::getHttpMethod()
+{
+    return this->_httpMethod;
+}
+std::string Request::getPath()
+{
+    return this->_path;
+}
+std::map<std::string, std::string> Request::getHeaders()
+{
+    return this->_headers;
+}
+std::string Request::getBody()
+{
+    return this->_body;
+}
+
+void Request::parseRequest(std::string request)
+{
+    try
+    {
+        this->_rawFirstLine = strtok((char *)request.c_str(), "\n");
+        this->_rawHeader = strtok((char *)request.c_str(), "\n");
+        char *headerKey;
+        char *headerValue;
+        headerKey = 
+        this->_headers.insert({strtok((char *)this->_rawHeader.c_str(), ": "), });
+        while()
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+}

b8b94b67df38a727c283a9e81c0999e653531863
Min kyeong Tak <mtak@c10r2s13.42seoul.kr>
2021-12-07 16:53:40 +0900

    curl http://localhost:8000/directory/youpi.bad_exte nsion 통과

--- core/Cluster.cpp
+++ core/Cluster.cpp
@@ -254,15 +254,16 @@ void Cluster::_makeRequestInfo(int idxServer, const std::string& cliReq) {
   _request.setRequest(cliReq);
   if (!_request.getPath().compare("/favicon.ico"))
     _request.setRequest(_request.getMethod() + " / " + _request.getHttpVersion());
-  const int statusCode = _response.getStatusCode();
-  if (_fileJudge(idxServer) == SUCCESS && statusCode == 200)
+  // const int statusCode = _response.getStatusCode();
+  if (_fileJudge(idxServer) == SUCCESS && _response.getStatusCode() == 200)
     return;
   _isDirectory(idxServer);
+  int statusCode = _response.getStatusCode();
   if (statusCode == 400 || statusCode == 403 || statusCode == 404 || statusCode == 405)
   {
-    int temp = statusCode;
+    // int temp = statusCode;
     _body = _setBody("400.html");
-    _response.setStatusCode(temp);
+    // _response.setStatusCode(temp);
   }
 }
 
@@ -284,7 +285,9 @@ std::string Cluster::_setBody(std::string file)
   if (!ifs) {
     Debug::log("file open error");
     _response.setStatusCode(404);
-  } else {
+  }
+  else 
+  {
     char c;
     while (ifs.get(c))
       body += c;

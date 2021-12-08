0a920f29b3e054ee1622519bf81df84f06083d73
jinchoi <znyous@gmail.com>
2021-12-08 16:56:42 +0900

    업로드 php 수정

--- YoupiBanane/upload.php
+++ YoupiBanane/upload.php
@@ -1,10 +1,7 @@
 <?php
-echo "a";
 $target_dir = "../upload/";
-echo "B";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 echo $_FILES["fileToUpload"]["name"];
-echo "C";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
@@ -32,21 +29,13 @@ if ($_FILES["fileToUpload"]["size"] > 5000000) {
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" && $imageFileType != "txt") {
 		echo $_FILES["fileToUpload"]["name"];
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+    }else {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
 }
-// Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
-		echo "<br><img src=/uploads/". basename( $_FILES["fileToUpload"]["name"]). ">";
-		echo "<br><button type='button' onclick='history.back()'>돌아가기</button>";
-    } else {
-        echo "<p>Sorry, there was an error uploading your file.</p>";
-		echo "<br><button type='button' onclick='history.back()'>돌아가기</button>";
-    }
+    echo "Upload Complete!!";
 }
 ?>
